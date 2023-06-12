c6fab5f97ca7ee94da0ad4112f78ce0eecdf6f75
Augusto Paz <augustoxx2015@gmail.com>
2023-06-12 14:17:52 -0300

    Modificaciones en model project y nuevo modelos de tecnologias

--- MyPortfolio/Portfolio/views.py
+++ MyPortfolio/Portfolio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Project
-
+from django.db.models import Q
 # Create your views here.
 def home(request):
     context={}
     context['projects'] = Project.objects.all()
+    context['Are_All_Not_Visible'] = not Project.objects.filter(~Q(visible=False)).exists()
     return render(request,'home.html',context=context)
