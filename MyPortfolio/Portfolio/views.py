da9ec5f6a5541d9ca2f5875e8d92cc84f07704bd
Augusto <augustoxx2015@gmail.com>
2023-07-18 01:12:10 -0300

    conexion model tech y template, mejoras en skills y tech

--- MyPortfolio/Portfolio/views.py
+++ MyPortfolio/Portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from Projects.models import Project
+from Projects.models import Project, Technologie
 from django.db.models import Q
 # Create your views here.
 def home(request):
     context={}
     context['projects'] = Project.objects.all()
+    context['technologies'] = Technologie.objects.all()
     context['Are_All_Not_Visible'] = not Project.objects.filter(~Q(visible=False)).exists()
     return render(request,'home.html',context=context)
